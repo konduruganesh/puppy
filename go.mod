18934cbdbf2b4171ef922315f40b855b381fb7a2
Ganesh Konduru <grajuk009@gmail.com>
2023-06-11 18:32:47 -0700

    to test third commit

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/konduruganesh/puppy
 
 go 1.20
+
+require github.com/konduruganesh/dog1 v0.0.0-20230612004750-00fcebc6bc0b
--- puppy.go
+++ puppy.go
@@ -1,9 +1,15 @@
 package puppy
 
+import dog1 "github.com/konduruganesh/dog1/com.dog"
+
 func Bark() string {
 	return "Woff!"
 }
 
 func Barks() string {
 	return "Woof! woof! woof!"
+}
+
+func BigBark() string {
+	return dog1.WHenGrownUp("Big Bark")
 }
